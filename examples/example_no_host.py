d765c5e768f9685efd4349e8f4ca3e813222635f
Addison <ha00871@cluster4.ph.surrey.ac.uk>
2025-11-03 12:15:50 +0000

    New example: no host contamination

---
+++ examples/example_no_host.py
@@ -0,0 +1,30 @@
+from host_removal import HostGalaxyRemoval
+from astropy.table import QTable
+
+# Load in the observed SN Ia spectrum
+sn_spec = QTable.read("./data/no_host_sn_spectrum.fits")
+
+# Define the needed properties of the SN.
+rest_phase = 0
+z = 0.01
+
+# Convert the SN spectrum to rest frame (i.e de-redshift it)
+sn_spec["wave"] /= (1 + z)
+
+# Normalise the SN spectrum
+max_flux = max(sn_spec["flux"])
+sn_spec["flux"] /= max_flux
+sn_spec["flux_err"] /= max_flux
+
+# Initialise the HostGalaxyRemoval object for this SN spectrum
+hgr = HostGalaxyRemoval(sn_spec, rest_phase, keys=["wave", "flux", "flux_err"])
+
+# Run the fitting procedure to get the galaxy model
+hgr.fit_spectrum()
+
+# Remove the host galaxy model from the observed spectrum.
+hgr.remove_galaxy_contamination()
+
+# Plot the results of the fitting
+hgr.plot_fit(plot_host_free_spec=True, plot_gal_components=True, show=True)
+
